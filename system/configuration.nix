39851c1f40be48659446b5def42afba019bdea57
rbo13 <chardyy.orofeo@gmail.com>
2023-05-12 10:11:43 +0800

    added keybase configuration

--- system/configuration.nix
+++ system/configuration.nix
@@ -165,6 +165,7 @@
     bintools-unwrapped
     fwupd
     gcc
+    keybase
     lua5_4
     luajit
     git
@@ -228,6 +229,34 @@
 
   # Enable tor service
   services.tor.client.enable = true;
+
+  # Enable KBFS service
+  services.kbfs = {
+    enable = true;
+    mountPoint = "%t/kbfs";
+    extraFlags = [ "-label %u" ];
+  };
+ 
+  # Enable systemd to support Keybase. 
+  systemd.user.services = {
+    keybase.serviceConfig.Slice = "keybase.slice";
+
+    kbfs = {
+      environment = { KEYBASE_RUN_MODE = "prod"; };
+      serviceConfig.Slice = "keybase.slice";
+    };
+
+    keybase-gui = {
+      description = "Keybase GUI";
+      requires = [ "keybase.service" "kbfs.service" ];
+      after    = [ "keybase.service" "kbfs.service" ];
+      serviceConfig = {
+        ExecStart  = "${pkgs.keybase-gui}/share/keybase/Keybase";
+        PrivateTmp = true;
+        Slice      = "keybase.slice";
+      };
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
